aa994f5b66925e1e39ead94fc4b1aaa7b5ea0285
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-05-02 01:01:44 +0530

    Small tweak.

--- DynMem/Headers/utility/utility.h
+++ DynMem/Headers/utility/utility.h
@@ -12,8 +12,7 @@ void DYNMEM_UTILITY_EXPORT DynMemUtilitySetMemoryBlock(uint8_t *destination, uin
 
 // source_size <= destination_size
 void DYNMEM_UTILITY_EXPORT DynMemUtilitySetMemory(void *destination, intmax_t destination_size,
-                                                  void *source, intmax_t source_size
-                                                 );
+                                                  void *source, intmax_t source_size);
 
 #endif  // DYNMEM_INTERNAL_USE_utility_utility_h
 

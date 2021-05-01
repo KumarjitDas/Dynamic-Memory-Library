12c24e870c67cb28eef1dcb02481891111ccecb3
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-05-01 11:28:20 +0530

    Small tweak in utility defines.

--- DynMem/Headers/utility/defines.h
+++ DynMem/Headers/utility/defines.h
@@ -22,10 +22,12 @@
 #define FALSE (_Bool)(0)
 #endif
 
-#define DYNMEM_UTILITY_VALIDATE(dynmem) (dynmem.es && dynmem.is && dynmem.cs && dynmem.csh && dynmem.m)
+#define DYNMEM_UTILITY_VALIDATE(dynmem) \
+   (dynmem.es > 0 && dynmem.is > 0 && dynmem.cs > 0 && dynmem.csh > 0 && dynmem.m != NULL)
 
-#define DYNMEM_UTILITY_VALIDATE_ADDRESS(dynmem_address) \
-   (dynmem_address->es && dynmem_address->is && dynmem_address->cs && dynmem_address->csh && dynmem_address->m)
+#define DYNMEM_UTILITY_VALIDATE_ADDRESS(dynmem_address)                              \
+   (dynmem_address->es > 0 && dynmem_address->is > 0 && dynmem_address->cs > 0 && \
+    dynmem_address->csh > 0 && dynmem_address->m != NULL)
 
 #define DYNMEM_UTILITY_RESET(dynmem) \
    {                                 \
@@ -50,40 +52,40 @@
    }
 
 #ifdef UINT64_MAX
-#define DYNMEM_UTILITY_ASSIGN(size, left_value, right_value)            \
-   {                                                                    \
-      switch (size) {                                                   \
-         case sizeof(uint8_t):                                          \
-            *(uint8_t *)left_value = *(uint8_t *)right_value;           \
-            break;                                                      \
-         case sizeof(uint16_t):                                         \
-            *(uint16_t *)left_value = *(uint16_t *)right_value;         \
-            break;                                                      \
-         case sizeof(uint32_t):                                         \
-            *(uint32_t *)left_value = *(uint32_t *)right_value;         \
-            break;                                                      \
-         case sizeof(uint64_t):                                         \
-            *(uint64_t *)left_value = *(uint64_t *)right_value;         \
-            break;                                                      \
-         default:                                                       \
-            DynMemUtilitySetMemoryBlock(left_value, right_value, size); \
-      }                                                                 \
+#define DYNMEM_UTILITY_ASSIGN(size, left_value, right_value)                \
+   {                                                                        \
+      switch (size) {                                                       \
+         case sizeof(uint8_t):                                              \
+            *(uint8_t *)(left_value) = *(uint8_t *)(right_value);           \
+            break;                                                          \
+         case sizeof(uint16_t):                                             \
+            *(uint16_t *)(left_value) = *(uint16_t *)(right_value);         \
+            break;                                                          \
+         case sizeof(uint32_t):                                             \
+            *(uint32_t *)(left_value) = *(uint32_t *)(right_value);         \
+            break;                                                          \
+         case sizeof(uint64_t):                                             \
+            *(uint64_t *)(left_value) = *(uint64_t *)(right_value);         \
+            break;                                                          \
+         default:                                                           \
+            DynMemUtilitySetMemoryBlock((left_value), (right_value), size); \
+      }                                                                     \
    }
 #else
-#define DYNMEM_UTILITY_ASSIGN(size, left_value, right_value)                        \
-   {                                                                                \
-      switch (size) {                                                               \
-         case sizeof(uint8_t):                                                      \
-            *(uint8_t *)left_value = *(uint8_t *)right_value;                       \
-            break;                                                                  \
-         case sizeof(uint16_t):                                                     \
-            *(uint16_t *)left_value = *(uint16_t *)right_value;                     \
-            break;                                                                  \
-         case sizeof(uint32_t):                                                     \
-            *(uint32_t *)left_value = *(uint32_t *)right_value;                     \
-            break;                                                                  \
-            \ default : DynMemUtilitySetMemoryBlock(left_value, right_value, size); \
-      }                                                                             \
+#define DYNMEM_UTILITY_ASSIGN(size, left_value, right_value)                            \
+   {                                                                                    \
+      switch (size) {                                                                   \
+         case sizeof(uint8_t):                                                          \
+            *(uint8_t *)(left_value) = *(uint8_t *)(right_value);                       \
+            break;                                                                      \
+         case sizeof(uint16_t):                                                         \
+            *(uint16_t *)(left_value) = *(uint16_t *)(right_value);                     \
+            break;                                                                      \
+         case sizeof(uint32_t):                                                         \
+            *(uint32_t *)(left_value) = *(uint32_t *)(right_value);                     \
+            break;                                                                      \
+            \ default : DynMemUtilitySetMemoryBlock((left_value), (right_value), size); \
+      }                                                                                 \
    }
 #endif  // UINT64_MAX
 

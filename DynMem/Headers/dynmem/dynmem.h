8bba3fa2765f18d6d41819987f24d420d0e0f418
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-05-02 00:08:21 +0530

    Adding DynMemReduce function.

--- DynMem/Headers/dynmem/dynmem.h
+++ DynMem/Headers/dynmem/dynmem.h
@@ -14,6 +14,8 @@ _Bool DYNMEM_EXPORT DynMemAllocate(dynmem_t *dynmem_address,
 
 _Bool DYNMEM_EXPORT DynMemDeallocate(dynmem_t *dynmem_address);
 
+_Bool DYNMEM_EXPORT DynMemReduce(dynmem_t *dynmem_address, void *memory_address, intmax_t *size_address);
+
 _Bool DYNMEM_EXPORT DynMemAppend(dynmem_t *dynmem_address, void *value_address);
 
 _Bool DYNMEM_EXPORT DynMemPrepend(dynmem_t *dynmem_address, void *value_address);

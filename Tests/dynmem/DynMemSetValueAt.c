7715286650922e223f8db6f5151f4a15033379b7
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-05-02 08:12:47 +0530

    Adding tests for DynMemSetValueAt function

--- Tests/dynmem/DynMemSetValueAt.c
+++ Tests/dynmem/DynMemSetValueAt.c
@@ -1,69 +1,103 @@
+#include "check.h"
 #include "dynmem/dynmem.h"
 
+START_TEST(null_dynmem_null_value) {
+   ck_assert_int_eq(DynMemSetValueAt(NULL, 0, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(NULL, 50, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(NULL, 999, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(NULL, -1, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(NULL, -50, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(NULL, -999, NULL), DYNMEM_FAILED);
+}
+END_TEST
+
+START_TEST(null_dynmem_nonnull_value) {
+   int value = 123;
+
+   ck_assert_int_eq(DynMemSetValueAt(NULL, 0, &value), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(NULL, 50, &value), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(NULL, 999, &value), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(NULL, -1, &value), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(NULL, -50, &value), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(NULL, -999, &value), DYNMEM_FAILED);
+}
+END_TEST
+
+START_TEST(nonnull_dynmem_null_value) {
+   dynmem_t dynmem;
+
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, 0, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, 50, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, 999, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, -1, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, -50, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, -999, NULL), DYNMEM_FAILED);
+}
+END_TEST
+
+START_TEST(nonnull_dynmem_nonnull_value_positive_index) {
+   dynmem_t dynmem;
+   int value;
+
+   ck_assert_int_eq(DynMemAllocate(&dynmem, sizeof(int), 2, NULL), DYNMEM_SUCCEED);
+   ck_assert_int_eq(DynMemPrepend(&dynmem, NULL), DYNMEM_SUCCEED);
+   ck_assert_int_eq(DynMemPrepend(&dynmem, NULL), DYNMEM_SUCCEED);
+   ck_assert_int_eq(DynMemAppend(&dynmem, NULL), DYNMEM_SUCCEED);
+   ck_assert_int_eq(DynMemAppend(&dynmem, NULL), DYNMEM_SUCCEED);
+
+   value = 123;
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, 0, &value), DYNMEM_SUCCEED);
+   ck_assert_int_eq(*((int *)dynmem.m), value);
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, 1, &value), DYNMEM_SUCCEED);
+   ck_assert_int_eq(*((int *)(dynmem.m + dynmem.es)), value);
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, 3, &value), DYNMEM_SUCCEED);
+   ck_assert_int_eq(*((int *)(dynmem.m + dynmem.es * 3)), value);
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, 999, &value), DYNMEM_FAILED);
+
+   ck_assert_int_eq(DynMemDeallocate(&dynmem), DYNMEM_SUCCEED);
+}
+END_TEST
+
+START_TEST(nonnull_dynmem_nonnull_value_negative_index) {
+   dynmem_t dynmem;
+   int value;
+
+   ck_assert_int_eq(DynMemAllocate(&dynmem, sizeof(int), 2, NULL), DYNMEM_SUCCEED);
+   ck_assert_int_eq(DynMemPrepend(&dynmem, NULL), DYNMEM_SUCCEED);
+   ck_assert_int_eq(DynMemPrepend(&dynmem, NULL), DYNMEM_SUCCEED);
+   ck_assert_int_eq(DynMemAppend(&dynmem, NULL), DYNMEM_SUCCEED);
+   ck_assert_int_eq(DynMemAppend(&dynmem, NULL), DYNMEM_SUCCEED);
+
+   value = 123;
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, -1, &value), DYNMEM_SUCCEED);
+   ck_assert_int_eq(*((int *)(dynmem.m + dynmem.es * 3)), value);
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, -3, &value), DYNMEM_SUCCEED);
+   ck_assert_int_eq(*((int *)(dynmem.m + dynmem.es)), value);
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, -4, &value), DYNMEM_SUCCEED);
+   ck_assert_int_eq(*((int *)dynmem.m), value);
+   ck_assert_int_eq(DynMemSetValueAt(&dynmem, -999, &value), DYNMEM_FAILED);
+
+   ck_assert_int_eq(DynMemDeallocate(&dynmem), DYNMEM_SUCCEED);
+}
+END_TEST
+
 int main() {
-    dynmem_t dynmem;
-    intmax_t length = 16;
+   Suite *suite = suite_create("Test suite for \"DynMemSetValueAt\" function");
+   TCase *test_cases = tcase_create("Test case");
 
-    if (DynMemAllocate(&dynmem, sizeof(int), length, NULL))
-        return 1;
+   tcase_add_test(test_cases, null_dynmem_null_value);
+   tcase_add_test(test_cases, null_dynmem_nonnull_value);
+   tcase_add_test(test_cases, nonnull_dynmem_null_value);
+   tcase_add_test(test_cases, nonnull_dynmem_nonnull_value_positive_index);
+   tcase_add_test(test_cases, nonnull_dynmem_nonnull_value_negative_index);
 
-    int value = 0;
-    for (intmax_t i = 0; i < length; i++)
-        if (DynMemAppend(&dynmem, &value))
-            return 1;
+   suite_add_tcase(suite, test_cases);
 
-    int result = 1;
+   SRunner *suite_runner = srunner_create(suite);
+   srunner_run_all(suite_runner, CK_VERBOSE);
 
+   int failed_test_case_numbers = srunner_ntests_failed(suite_runner);
+   srunner_free(suite_runner);
 
-    // Test 1
-
-    value = 69;
-    result &= DynMemSetValueAt(&dynmem, 0, &value) == DYNMEM_SUCCEED;
-    result &= DYNMEM_VALUE_AT(int, dynmem, 0) == value;
-
-
-    // Test 2
-
-    value = 420;
-    result &= DynMemSetValueAt(&dynmem, length - 1, &value) == DYNMEM_SUCCEED;
-    result &= DYNMEM_VALUE_AT(int, dynmem, length - 1) == value;
-
-
-    // Test 3
-
-    value = 42069;
-    result &= DynMemSetValueAt(&dynmem, length / 2, &value) == DYNMEM_SUCCEED;
-    result &= DYNMEM_VALUE_AT(int, dynmem, length / 2) == value;
-
-
-    // Test 4
-
-    value = 123456;
-    result &= DynMemSetValueAt(&dynmem, length + 2, &value) == DYNMEM_FAILED;
-
-
-    // Test 5
-
-    value = 69;
-    result &= DynMemSetValueAt(&dynmem, -1, &value) == DYNMEM_SUCCEED;
-    result &= DYNMEM_VALUE_AT(int, dynmem, length - 1) == value;
-
-
-    // Test 6
-
-    value = 420;
-    result &= DynMemSetValueAt(&dynmem, -3, &value) == DYNMEM_SUCCEED;
-    result &= DYNMEM_VALUE_AT(int, dynmem, length - 3) == value;
-
-
-    // Test 7
-
-    value = 42069;
-    result &= DynMemSetValueAt(&dynmem, -100, &value) == DYNMEM_FAILED;
-
-
-    if (DynMemDeallocate(&dynmem))
-        return 1;
-
-    return !result;
+   return failed_test_case_numbers;
 }

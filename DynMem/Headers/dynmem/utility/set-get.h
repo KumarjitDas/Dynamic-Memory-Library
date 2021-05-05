35cbd9984b27ec7af450e642c7e06ef7fe9fd346
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-05-05 15:33:49 +0530

    Adding DynMemUtilitySet function

---
+++ DynMem/Headers/dynmem/utility/set-get.h
@@ -0,0 +1,14 @@
+#ifndef DYNMEM_UTILITY_SET_GET_H
+#define DYNMEM_UTILITY_SET_GET_H
+
+#ifdef DYNMEM_INTERNAL_USE_UTILITY_SET_GET_H
+
+#include "dynmem/common/defines.h"
+#include "dynmem/common/standard_includes.h"
+#include "dynmem/common/types.h"
+
+_Bool DYNMEM_UTILITY_EXPORT DynMemUtilitySet(dynmem_t *dynmem_address, intmax_t index, void *value_address);
+
+#endif  // DYNMEM_INTERNAL_USE_UTILITY_SET_GET_H
+
+#endif  // DYNMEM_UTILITY_SET_GET_H
---
+++ DynMem/Sources/dynmem/utility/set-get.c
@@ -0,0 +1,23 @@
+#ifndef DYNMEM_INTERNAL_USE_UTILITY_DEFINES_H
+#define DYNMEM_INTERNAL_USE_UTILITY_DEFINES_H
+#endif
+#ifndef DYNMEM_INTERNAL_USE_UTILITY_SET_GET_H
+#define DYNMEM_INTERNAL_USE_UTILITY_SET_GET_H
+#endif
+
+#include "dynmem/utility/defines.h"
+#include "dynmem/utility/set-get.h"
+
+_Bool DynMemUtilitySet(dynmem_t *dynmem_address, intmax_t index, void *value_address) {
+   index += dynmem_address->bi;
+
+   if (index < 0) index += (dynmem_address->ei + dynmem_address->es);
+
+   if (index < dynmem_address->bi || index > dynmem_address->ei)
+      return DYNMEM_FAILED;
+
+   void *destination = dynmem_address->m + index;
+   DYNMEM_UTILITY_ASSIGN(dynmem_address->es, destination, value_address);
+
+   return DYNMEM_SUCCEED;
+}

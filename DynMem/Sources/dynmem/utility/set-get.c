ea7aff41a392ba1951ce123c223ea13e87f489f5
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-05-06 08:51:09 +0530

    Adding DynMemUtilityGetValues function

--- DynMem/Sources/dynmem/utility/set-get.c
+++ DynMem/Sources/dynmem/utility/set-get.c
@@ -1,11 +1,15 @@
 #ifndef DYNMEM_INTERNAL_USE_UTILITY_DEFINES_H
 #define DYNMEM_INTERNAL_USE_UTILITY_DEFINES_H
 #endif
+#ifndef DYNMEM_INTERNAL_USE_UTILITY_UTILITY_H
+#define DYNMEM_INTERNAL_USE_UTILITY_UTILITY_H
+#endif
 #ifndef DYNMEM_INTERNAL_USE_UTILITY_SET_GET_H
 #define DYNMEM_INTERNAL_USE_UTILITY_SET_GET_H
 #endif
 
 #include "dynmem/utility/defines.h"
+#include "dynmem/utility/utility.h"
 #include "dynmem/utility/set-get.h"
 
 _Bool DynMemUtilitySet(dynmem_t *dynmem_address, intmax_t index, void *value_address) {
@@ -53,6 +57,39 @@ _Bool DynMemUtilitySetValues(dynmem_t *dynmem_address, intmax_t begin, intmax_t 
       return DYNMEM_FAILED;
 
    DynMemUtilitySetMemory(dynmem_address->m + begin, end - begin, value_address, dynmem_address->es);
+
+   return DYNMEM_SUCCEED;
+}
+
+_Bool DYNMEM_UTILITY_EXPORT DynMemUtilityGetValues(dynmem_t *dynmem_address, intmax_t begin, intmax_t end,
+                                                   void *array, intmax_t size, intmax_t *got_values) {
+   if (got_values != NULL)
+      *got_values = 0;
+
+   begin += dynmem_address->bi;
+   intmax_t end_offset = dynmem_address->ei + dynmem_address->es;
+
+   if (begin < 0) begin += end_offset;
+
+   if (begin < dynmem_address->bi || begin > dynmem_address->ei)
+      return DYNMEM_FAILED;
+
+   end += dynmem_address->bi;
+
+   if (end < 0) end += (end_offset + dynmem_address->es);
+
+   if (end <= begin || end < dynmem_address->bi || end > end_offset)
+      return DYNMEM_FAILED;
+
+   intmax_t temporary_size = end - begin;
+
+   if (size <= 0 || size > temporary_size)
+      size = temporary_size;
+
+   DynMemUtilitySetMemoryBlock(array, dynmem_address->m + begin, size);
+
+   if (got_values != NULL)
+      *got_values = size;
 
    return DYNMEM_SUCCEED;
 }

e5ab4f9dbe5ff6a2480c7c0834e384620e5e8198
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-05-09 11:14:10 +0530

    Adding DynMemUtilityPrepend function

--- DynMem/Headers/dynmem/utility/stack-queue.h
+++ DynMem/Headers/dynmem/utility/stack-queue.h
@@ -9,6 +9,8 @@
 
 _Bool DYNMEM_UTILITY_EXPORT DynMemUtilityAppend(dynmem_t *dynmem_address, void *memory, intmax_t size);
 
+_Bool DYNMEM_UTILITY_EXPORT DynMemUtilityPrepend(dynmem_t *dynmem_address, void *memory, intmax_t size);
+
 #endif  // DYNMEM_INTERNAL_USE_UTILITY_STACK_QUEUE_H
 
 #endif  // DYNMEM_UTILITY_STACK_QUEUE_H
--- DynMem/Sources/dynmem/utility/stack-queue.c
+++ DynMem/Sources/dynmem/utility/stack-queue.c
@@ -40,3 +40,37 @@ _Bool DynMemUtilityAppend(dynmem_t *dynmem_address, void *memory, intmax_t size)
 
    return DYNMEM_SUCCEED;
 }
+
+_Bool DynMemUtilityPrepend(dynmem_t *dynmem_address, void *memory, intmax_t size) {
+   intmax_t beginning_index = dynmem_address->bi - size;
+
+   if (beginning_index < 0) {
+      intmax_t temporary_size = dynmem_address->cs + (-beginning_index);
+      intmax_t half_size = dynmem_address->cs;
+      dynmem_address->cs *= 2;
+
+      while (dynmem_address->cs < temporary_size) {
+         dynmem_address->cs *= 2;
+         half_size *= 2;
+      }
+
+      dynmem_address->m = realloc(dynmem_address->m, dynmem_address->cs);
+
+      if (dynmem_address->m == NULL) {
+         DYNMEM_UTILITY_RESET_ADDRESS(dynmem_address);
+         return DYNMEM_FAILED;
+      }
+
+      DynMemUtilitySetMemoryBlock(dynmem_address->m + half_size, dynmem_address->m,
+                                  dynmem_address->ei + dynmem_address->es);
+      dynmem_address->ei += half_size - dynmem_address->bi;
+      dynmem_address->bi = half_size;
+   }
+
+   if (memory == NULL) return DYNMEM_SUCCEED;
+
+   uint8_t *destination = dynmem_address->m + dynmem_address->bi - size;
+   DYNMEM_UTILITY_ASSIGN(size, destination, memory);
+
+   return DYNMEM_SUCCEED;
+}

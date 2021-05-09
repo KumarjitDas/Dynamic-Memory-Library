917234d0b127a1506218b5a5baf7fe82f706912f
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-05-09 16:29:13 +0530

    Removing wrong tests

--- Tests/dynmem/DynMemAllocate.c
+++ Tests/dynmem/DynMemAllocate.c
@@ -1,132 +1,100 @@
 #include "check.h"
 #include "dynmem/dynmem.h"
 
-START_TEST(null_dynmem_without_preallocated_memory) {
-   intmax_t length = 5;
-   intmax_t element_size = sizeof(int);
+#include <stdlib.h>
 
+START_TEST(null_dynmem__without_preallocated_memory) {
    ck_assert_int_eq(DynMemAllocate(NULL, 0, 0, NULL), DYNMEM_FAILED);
-   ck_assert_int_eq(DynMemAllocate(NULL, 0, length, NULL), DYNMEM_FAILED);
-   ck_assert_int_eq(DynMemAllocate(NULL, element_size, 0, NULL), DYNMEM_FAILED);
-   ck_assert_int_eq(DynMemAllocate(NULL, element_size, length, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemAllocate(NULL, 0, 5, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemAllocate(NULL, 4, 0, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemAllocate(NULL, 4, 5, NULL), DYNMEM_FAILED);
 }
 END_TEST
 
-START_TEST(nonnull_dynmem_zero_size_without_preallocated_memory) {
-   dynmem_t dynmem;
-   intmax_t length = 5;
-   intmax_t element_size = sizeof(int);
-
-   ck_assert_int_eq(DynMemAllocate(&dynmem, 0, 0, NULL), DYNMEM_FAILED);
-   ck_assert_int_eq(DynMemAllocate(&dynmem, 0, length, NULL), DYNMEM_FAILED);
-   ck_assert_int_eq(DynMemAllocate(&dynmem, element_size, 0, NULL), DYNMEM_FAILED);
-}
-END_TEST
-
-START_TEST(nonnull_dynmem_nonzero_size_without_preallocated_memory) {
-   dynmem_t dynmem;
-   intmax_t length = 5;
-   intmax_t element_size = sizeof(int);
-   intmax_t size = length * element_size;
-
-   ck_assert_int_eq(DynMemAllocate(&dynmem, element_size, length, NULL), DYNMEM_SUCCEED);
-   ck_assert_int_eq(dynmem.es, element_size);
-   ck_assert_int_eq(dynmem.is, size);
-   ck_assert_int_eq(dynmem.cs, size * 2);
-   ck_assert_int_eq(dynmem.bi, size);
-   ck_assert_int_eq(dynmem.ei, size - element_size);
-   ck_assert_ptr_nonnull(dynmem.m);
-
-   free(dynmem.m);
-}
-END_TEST
-
-START_TEST(nonnull_dynmem_small_size_without_preallocated_memory) {
-   dynmem_t dynmem;
-   intmax_t length = 1;
-   intmax_t element_size = 3;
-   intmax_t size = ((sizeof(intmax_t) + element_size) / element_size) * element_size;
-
-   ck_assert_int_eq(DynMemAllocate(&dynmem, element_size, length, NULL), DYNMEM_SUCCEED);
-   ck_assert_int_eq(dynmem.es, element_size);
-   ck_assert_int_eq(dynmem.is, size);
-   ck_assert_int_eq(dynmem.cs, size * 2);
-   ck_assert_int_eq(dynmem.bi, size);
-   ck_assert_int_eq(dynmem.ei, size - element_size);
-   ck_assert_ptr_nonnull(dynmem.m);
-
-   free(dynmem.m);
-}
-END_TEST
-
-START_TEST(null_dynmem_with_allocated_memory) {
-   intmax_t length = 5;
-   intmax_t element_size = sizeof(int);
-   intmax_t size = length * element_size;
-   int *memory = malloc(size);
+START_TEST(null_dynmem__with_preallocated_memory) {
+   int *memory = malloc(4);
 
    ck_assert_int_eq(DynMemAllocate(NULL, 0, 0, &memory), DYNMEM_FAILED);
-   ck_assert_int_eq(DynMemAllocate(NULL, 0, length, &memory), DYNMEM_FAILED);
-   ck_assert_int_eq(DynMemAllocate(NULL, element_size, 0, &memory), DYNMEM_FAILED);
-   ck_assert_int_eq(DynMemAllocate(NULL, element_size, length, &memory), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemAllocate(NULL, 0, 5, &memory), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemAllocate(NULL, 4, 0, &memory), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemAllocate(NULL, 4, 5, &memory), DYNMEM_FAILED);
 
    if (memory != NULL) free(memory);
 }
 END_TEST
 
-START_TEST(nonnull_dynmem_zero_size_with_allocated_memory) {
+START_TEST(nonnull_dynmem__without_preallocated_memory) {
    dynmem_t dynmem;
-   intmax_t length = 5;
-   intmax_t element_size = sizeof(int);
-   intmax_t size = length * element_size;
-   int *memory = malloc(size);
-
-   ck_assert_int_eq(DynMemAllocate(&dynmem, 0, 0, &memory), DYNMEM_FAILED);
-   ck_assert_int_eq(DynMemAllocate(&dynmem, 0, length, &memory), DYNMEM_FAILED);
-   ck_assert_int_eq(DynMemAllocate(&dynmem, element_size, 0, &memory), DYNMEM_FAILED);
-
-   if (memory != NULL) free(memory);
-}
-END_TEST
-
-START_TEST(nonnull_dynmem_nonzero_size_with_allocated_memory) {
-   dynmem_t dynmem;
-   intmax_t length = 2;
-   intmax_t element_size = 3;
-   intmax_t size = length * element_size;
-   int *memory = malloc(size);
-   int *temporary_memory = memory;
-
-   ck_assert_int_eq(DynMemAllocate(&dynmem, element_size, length, &memory), DYNMEM_SUCCEED);
-
-   size /= 2;
-   ck_assert_int_eq(dynmem.es, element_size);
-   ck_assert_int_eq(dynmem.is, size);
-   ck_assert_int_eq(dynmem.cs, size * 2);
-   ck_assert_int_eq(dynmem.bi, size);
-   ck_assert_int_eq(dynmem.ei, size - element_size);
+   ck_assert_int_eq(DynMemAllocate(&dynmem, 0, 0, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemAllocate(&dynmem, 0, 5, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemAllocate(&dynmem, 4, 0, NULL), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemAllocate(&dynmem, 4, 5, NULL), DYNMEM_SUCCEED);
+   ck_assert_int_eq(dynmem.es, 4);
+   ck_assert_int_eq(dynmem.is, 20);
+   ck_assert_int_eq(dynmem.cs, 40);
+   ck_assert_int_eq(dynmem.bi, 20);
+   ck_assert_int_eq(dynmem.ei, 16);
    ck_assert_ptr_nonnull(dynmem.m);
-   ck_assert_ptr_eq(dynmem.m, temporary_memory);
-   ck_assert_ptr_null(memory);
 
    free(dynmem.m);
 }
 END_TEST
 
-START_TEST(nonnull_dynmem_small_size_with_null_memory) {
+START_TEST(nonnull_dynmem__without_preallocated_memory__smaller_size) {
    dynmem_t dynmem;
-   intmax_t length = 5;
-   intmax_t element_size = sizeof(int);
-   intmax_t size = length * element_size;
+   ck_assert_int_eq(DynMemAllocate(&dynmem, 4, 1, NULL), DYNMEM_SUCCEED);
+   ck_assert_int_eq(dynmem.es, 4);
+
+#ifdef INT64_MAX
+   ck_assert_int_eq(dynmem.is, 12);
+   ck_assert_int_eq(dynmem.cs, 24);
+   ck_assert_int_eq(dynmem.bi, 12);
+   ck_assert_int_eq(dynmem.ei, 8);
+#else  // INT64_MAX
+   ck_assert_int_eq(dynmem.is, 8);
+   ck_assert_int_eq(dynmem.cs, 16);
+   ck_assert_int_eq(dynmem.bi, 8);
+   ck_assert_int_eq(dynmem.ei, 4);
+#endif  // INT64_MAX
+
+   ck_assert_ptr_nonnull(dynmem.m);
+   free(dynmem.m);
+}
+END_TEST
+
+START_TEST(nonnull_dynmem__with_null_memory) {
+   dynmem_t dynmem;
    int *memory = NULL;
 
-   ck_assert_int_eq(DynMemAllocate(&dynmem, element_size, length, &memory), DYNMEM_SUCCEED);
-   ck_assert_int_eq(dynmem.es, element_size);
-   ck_assert_int_eq(dynmem.is, size);
-   ck_assert_int_eq(dynmem.cs, size * 2);
-   ck_assert_int_eq(dynmem.bi, size);
-   ck_assert_int_eq(dynmem.ei, size - element_size);
+   ck_assert_int_eq(DynMemAllocate(&dynmem, 4, 5, &memory), DYNMEM_SUCCEED);
+   ck_assert_int_eq(dynmem.es, 4);
+   ck_assert_int_eq(dynmem.is, 20);
+   ck_assert_int_eq(dynmem.cs, 40);
+   ck_assert_int_eq(dynmem.bi, 20);
+   ck_assert_int_eq(dynmem.ei, 16);
    ck_assert_ptr_nonnull(dynmem.m);
+
+   free(dynmem.m);
+}
+END_TEST
+
+START_TEST(nonnull_dynmem__with_preallocated_memory) {
+   dynmem_t dynmem;
+   int *memory = malloc(20);
+   int *temporary_memory = memory;
+
+   ck_assert_int_eq(DynMemAllocate(&dynmem, 0, 0, &memory), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemAllocate(&dynmem, 0, 5, &memory), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemAllocate(&dynmem, 4, 0, &memory), DYNMEM_FAILED);
+   ck_assert_int_eq(DynMemAllocate(&dynmem, 4, 5, &memory), DYNMEM_SUCCEED);
+   ck_assert_int_eq(dynmem.es, 4);
+   ck_assert_int_eq(dynmem.is, 8);
+   ck_assert_int_eq(dynmem.cs, 16);
+   ck_assert_int_eq(dynmem.bi, 8);
+   ck_assert_int_eq(dynmem.ei, 4);
+   ck_assert_ptr_nonnull(dynmem.m);
+   ck_assert_ptr_null(memory);
+   ck_assert_ptr_eq(dynmem.m, temporary_memory);
 
    free(dynmem.m);
 }
@@ -136,15 +104,12 @@ int main() {
    Suite *suite = suite_create("Test suite for \"DynMemAllocate\" function");
    TCase *test_cases = tcase_create("Test case");
 
-   tcase_add_test(test_cases, null_dynmem_without_preallocated_memory);
-   tcase_add_test(test_cases, nonnull_dynmem_zero_size_without_preallocated_memory);
-   tcase_add_test(test_cases, nonnull_dynmem_nonzero_size_without_preallocated_memory);
-   tcase_add_test(test_cases, nonnull_dynmem_small_size_without_preallocated_memory);
-
-   tcase_add_test(test_cases, null_dynmem_with_allocated_memory);
-   tcase_add_test(test_cases, nonnull_dynmem_zero_size_with_allocated_memory);
-   tcase_add_test(test_cases, nonnull_dynmem_nonzero_size_with_allocated_memory);
-   tcase_add_test(test_cases, nonnull_dynmem_small_size_with_null_memory);
+   tcase_add_test(test_cases, null_dynmem__without_preallocated_memory);
+   tcase_add_test(test_cases, null_dynmem__with_preallocated_memory);
+   tcase_add_test(test_cases, nonnull_dynmem__without_preallocated_memory);
+   tcase_add_test(test_cases, nonnull_dynmem__without_preallocated_memory__smaller_size);
+   tcase_add_test(test_cases, nonnull_dynmem__with_null_memory);
+   tcase_add_test(test_cases, nonnull_dynmem__with_preallocated_memory);
 
    suite_add_tcase(suite, test_cases);
 
